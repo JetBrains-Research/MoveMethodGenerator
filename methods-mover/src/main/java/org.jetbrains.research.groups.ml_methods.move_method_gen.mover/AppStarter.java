9c78cae78e15dcebcd4fe9ccfb3904d437525f58
Ivan Veselov <veselov.iva@gmail.com>
2019-03-11 12:54:55 +0300

    Configure plugin in methods mover module.

--- methods-mover/src/main/java/org.jetbrains.research.groups.ml_methods.move_method_gen.mover/AppStarter.java
+++ methods-mover/src/main/java/org.jetbrains.research.groups.ml_methods.move_method_gen.mover/AppStarter.java
@@ -1,13 +1,22 @@
 package org.jetbrains.research.groups.ml_methods.move_method_gen.mover;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiMethod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.research.groups.ml_methods.move_method_gen.ProjectAppStarter;
+
+import java.util.List;
+
+import static org.jetbrains.research.groups.ml_methods.move_method_gen.utils.ExtractingUtils.extractClasses;
+import static org.jetbrains.research.groups.ml_methods.move_method_gen.utils.ExtractingUtils.extractMethods;
+import static org.jetbrains.research.groups.ml_methods.move_method_gen.utils.ExtractingUtils.extractSourceJavaFiles;
 
 public class AppStarter extends ProjectAppStarter {
     @Override
     public String getCommandName() {
-        return "change-project";
+        return "methods-mover";
     }
 
     @Override
@@ -23,5 +32,14 @@ public class AppStarter extends ProjectAppStarter {
 
     @Override
     protected void run(@NotNull Project project) throws Exception {
+        List<PsiJavaFile> javaFiles = extractSourceJavaFiles(project);
+        List<PsiClass> javaClasses = extractClasses(javaFiles);
+        List<PsiMethod> javaMethods = extractMethods(javaClasses);
+
+        PsiMethod method = javaMethods.stream().filter(it -> it.getName().equals("foo")).findFirst().get();
+        PsiClass clazz = javaClasses.stream().filter(it -> it.getName().equals("B")).findFirst().get();
+
+        System.out.println(method.getName());
+        System.out.println(clazz.getQualifiedName());
     }
 }
